d93b30513148e71b8ccc85a5925afcb32f9aecb3
Romesh Singhabahu <demo@imac-romesh.lan>
2014-07-28 18:38:42 +0200

    Initial Commit

---
+++ PadovaNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PadovaNews
+//
+//  Created by Romesh Singhabahu on 28/07/14.
+//  Copyright (c) 2014 Romesh Singhabahu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
