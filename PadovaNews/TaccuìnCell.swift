8c93dc8203423fc4c28b4ee7280fb0ab80adcacc
Romesh Singhabahu <demo@imac-Romesh.lan>
2015-01-16 14:10:30 +0100

    second commit

---
+++ PadovaNews/TaccuìnCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaccuìnCell.swift
+//  PadovaNews
+//
+//  Created by Romesh Singhabahu on 15/01/15.
+//  Copyright (c) 2015 Romesh Singhabahu. All rights reserved.
+//
+
+import UIKit
+
+class Taccui_nCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
